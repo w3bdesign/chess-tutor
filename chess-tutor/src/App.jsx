13c87910f8f2b70ca4a8a140405ac96e385ddbe7
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-10-02 01:01:04 +0200

    Layout update

--- chess-tutor/src/App.jsx
+++ chess-tutor/src/App.jsx
@@ -5,13 +5,13 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="flex flex-col p-4">
-        <div className="shadow border bg-white rounded w-full">
+      <div className="flex flex-col p-2">
+        <div className="shadow border bg-white rounded w-full min-w-[25rem]">
           <h1 className="text-2xl font-bold p-2">Chess Tutor</h1>
         </div>
       </div>
-      <div className="flex flex-col p-4">
-        <div className="shadow border bg-white rounded p-4 w-full">Test</div>
+      <div className="flex flex-col p-2">
+        <div className="shadow border bg-white rounded p-4 w-full min-w-[25rem] min-h-[20rem]">Test</div>
       </div>
     </>
   );
