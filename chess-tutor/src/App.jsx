071b66ebee253761d57bfc8c0baef17fb5647eb3
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-10-02 00:58:54 +0200

    Basic layout

---
+++ chess-tutor/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col p-4">
+        <div className="shadow border bg-white rounded w-full">
+          <h1 className="text-2xl font-bold p-2">Chess Tutor</h1>
+        </div>
+      </div>
+      <div className="flex flex-col p-4">
+        <div className="shadow border bg-white rounded p-4 w-full">Test</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
