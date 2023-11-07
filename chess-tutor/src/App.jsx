d2febdf1cc735b11522803927ed111f16795cf9c
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-11-08 00:25:04 +0100

    Chess.js

--- chess-tutor/src/App.jsx
+++ chess-tutor/src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { Chessboard2 } from "/node_modules/@chrisoakman/chessboard2/dist/chessboard2.min.mjs";
+import { Chess } from 'chess.js'
 
 import "./App.css";
 import "/node_modules/@chrisoakman/chessboard2/dist/chessboard2.min.css";
@@ -7,13 +8,21 @@ import "/node_modules/@chrisoakman/chessboard2/dist/chessboard2.min.css";
 function App() {
   useEffect(() => {
     const board = Chessboard2("myBoard", "start");
-  });
+    
+    const chess = new Chess()
+    while (!chess.isGameOver()) {
+      const moves = chess.moves()
+      const move = moves[Math.floor(Math.random() * moves.length)]
+      chess.move(move)
+    }
+    console.log(chess.pgn())
+  }, []);
 
   return (
     <>
       <div className="flex flex-col p-2">
         <div className="shadow border bg-white rounded w-full min-w-[25rem]">
-          <h1 className="text-2xl font-bold p-2">Chess Tutor</h1>
+          <h1 className="text-2xl font-bold p-4">Chess Tutor</h1>
         </div>
       </div>
       <div className="flex flex-col p-2">
