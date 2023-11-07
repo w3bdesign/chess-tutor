29cc5f13f3a849029de39542cd6ff95c08ff439f
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-11-08 00:15:29 +0100

    Chess board

--- chess-tutor/src/App.jsx
+++ chess-tutor/src/App.jsx
@@ -1,8 +1,14 @@
-import { useState } from "react";
+import { useEffect } from "react";
+import { Chessboard2 } from "/node_modules/@chrisoakman/chessboard2/dist/chessboard2.min.mjs";
 
 import "./App.css";
+import "/node_modules/@chrisoakman/chessboard2/dist/chessboard2.min.css";
 
 function App() {
+  useEffect(() => {
+    const board = Chessboard2("myBoard", "start");
+  });
+
   return (
     <>
       <div className="flex flex-col p-2">
@@ -11,7 +17,10 @@ function App() {
         </div>
       </div>
       <div className="flex flex-col p-2">
-        <div className="shadow border bg-white rounded p-4 w-full min-w-[25rem] min-h-[20rem]">Test</div>
+        <div
+          id="myBoard"
+          className="shadow border bg-white rounded p-4 w-full min-w-[40rem] min-h-[35rem]"
+        ></div>
       </div>
     </>
   );
