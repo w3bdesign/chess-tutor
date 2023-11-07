43d6f7575fac3fe2db33578158d5eb89b0ccbd2a
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-11-08 00:38:40 +0100

    Update App.jsx

--- chess-tutor/src/App.jsx
+++ chess-tutor/src/App.jsx
@@ -1,21 +1,52 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { Chessboard2 } from "/node_modules/@chrisoakman/chessboard2/dist/chessboard2.min.mjs";
-import { Chess } from 'chess.js'
+import { Chess } from "chess.js";
 
 import "./App.css";
 import "/node_modules/@chrisoakman/chessboard2/dist/chessboard2.min.css";
 
 function App() {
+  const chessboardRef = useRef(null);
+  const chessRef = useRef(new Chess());
+
   useEffect(() => {
-    const board = Chessboard2("myBoard", "start");
-    
-    const chess = new Chess()
-    while (!chess.isGameOver()) {
-      const moves = chess.moves()
-      const move = moves[Math.floor(Math.random() * moves.length)]
-      chess.move(move)
-    }
-    console.log(chess.pgn())
+    const boardConfig = {
+      draggable: true,
+      position: "start",
+      onDrop: (source, target) => {
+        // Attempt to make a move
+        const move = chessRef.current.move({
+          from: source,
+          to: target,
+          promotion: "q", // NOTE: Always promote to a queen for example simplicity
+        });
+
+        // Illegal move
+        if (move === null) return "snapback";
+
+        // Update the board position after the piece snap
+        // For castling, en passant, pawn promotion
+        chessboardRef.current.position(chessRef.current.fen());
+
+        // Check if the game is over
+
+        /*if (chessRef.current.game_over()) {
+          console.log('Game over');
+        }*/
+        
+      },
+    };
+
+    chessboardRef.current = Chessboard2("myBoard", boardConfig);
+
+    // Random move logic (for demonstration purposes)
+
+    return () => {
+      // Clean up the chessboard if the component unmounts
+      if (chessboardRef.current) {
+        chessboardRef.current.destroy();
+      }
+    };
   }, []);
 
   return (
@@ -27,6 +58,7 @@ function App() {
       </div>
       <div className="flex flex-col p-2">
         <div
+          //ref={chessboardRef}
           id="myBoard"
           className="shadow border bg-white rounded p-4 w-full min-w-[40rem] min-h-[35rem]"
         ></div>
