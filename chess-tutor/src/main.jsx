4d7e9f0b4dd89bb0f694f617c3ba6823cbf81fae
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-11-08 02:38:56 +0100

    Stockfish as API

--- chess-tutor/src/main.jsx
+++ chess-tutor/src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import App from "./App.jsx";
+import "./index.css";
+
+// Create a client
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
