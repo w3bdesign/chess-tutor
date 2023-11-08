5887bcfb1b69986a0bfaae73242a11e9dfcd6331
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-11-08 06:49:46 +0100

    Refactor

--- chess-tutor/src/App.jsx
+++ chess-tutor/src/App.jsx
@@ -1,83 +1,23 @@
-import { useEffect, useRef, useState } from "react";
-import { Chessboard2 } from "@chrisoakman/chessboard2/dist/chessboard2.min.mjs";
-import { Chess } from "chess.js";
-import useStockfishEvaluation from "./hooks/useStockfishEvaluation";
+import { useState } from "react";
 
+import ChessBoard from "./components/ChessBoard";
 import MoveHistory from "./components/MoveHistory";
 
 import "./App.css";
 import "@chrisoakman/chessboard2/dist/chessboard2.min.css";
 
 function App() {
-  const chessboardRef = useRef(null);
-  const chessRef = useRef(new Chess());
-
-  const [moveHistory, setMoveHistory] = useState([]);
   const [movePairs, setMovePairs] = useState([]);
-
-  const {
-    data: evaluation,
-    isError,
-    error,
-  } = useStockfishEvaluation(chessRef.current.fen());
-
-  useEffect(() => {
-    const pairs = [];
-    for (let i = 0; i < moveHistory.length; i += 2) {
-      pairs.push({
-        white: moveHistory[i],
-        black: moveHistory[i + 1] || null,
-      });
-    }
-    setMovePairs(pairs);
-  }, [moveHistory]);
-
-  useEffect(() => {
-    const boardConfig = {
-      draggable: true,
-      position: "start",
-      showNotation: true,
-      onDrop: ({ source, target }) => {
-        const move = chessRef.current.move({
-          from: source,
-          to: target,
-          promotion: "q",
-        });
-
-        if (move === null) return "snapback";
-
-        chessboardRef.current.position(chessRef.current.fen());
-        setMoveHistory(chessRef.current.history({ verbose: true }));
-      },
-    };
-
-    chessboardRef.current = Chessboard2("myBoard", boardConfig);
-
-    return () => {
-      if (chessboardRef.current) {
-        console.log("chessboardRef.current", chessboardRef.current);
-        console.log("Destroying chessboard");
-        
-        //chessboardRef.current.destroy();
-      }
-    };
-  }, []);
 
   return (
     <>
       <div className="flex flex-col p-2">
         <div className="shadow border bg-white rounded w-full min-w-[25rem]">
           <h1 className="text-2xl font-bold p-4">Chess Tutor</h1>
-          <p className="text-lg p-4 font-semibold">
-            Stockfish evaluation: {isError ? error.message : evaluation}
-          </p>
         </div>
       </div>
       <div className="flex flex-col p-2">
-        <div
-          id="myBoard"
-          className="shadow border bg-white rounded p-4 w-full min-w-[40rem] min-h-[35rem]"
-        ></div>
+        <ChessBoard setMovePairs={setMovePairs} />
         <MoveHistory movePairs={movePairs} />
       </div>
     </>
