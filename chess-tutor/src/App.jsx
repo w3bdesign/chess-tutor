4390969b1dcee2eca0f4033450b5663af1821970
Daniel <45217974+w3bdesign@users.noreply.github.com>
2024-07-11 05:42:45 +0200

    Update App.jsx

--- chess-tutor/src/App.jsx
+++ chess-tutor/src/App.jsx
@@ -12,7 +12,6 @@ function App() {
       <div className="shadow border bg-white rounded w-full mb-4">
         <h1 className="text-2xl font-bold p-4">Chess Tutor</h1>
       </div>
-
       <div className="flex flex-col lg:flex-row gap-4">
         <div className="lg:w-2/3">
           <ChessBoard setMovePairs={setMovePairs} />
