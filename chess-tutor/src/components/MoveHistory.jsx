2f6393669c29c9a1c40d4203508819eeef8080a7
Daniel <45217974+w3bdesign@users.noreply.github.com>
2024-07-11 05:27:02 +0200

    Update MoveHistory.jsx

--- chess-tutor/src/components/MoveHistory.jsx
+++ chess-tutor/src/components/MoveHistory.jsx
@@ -42,8 +42,7 @@ const formatMove = (move) => {
  */
 const MoveHistory = ({ movePairs }) => {
   return (
-    <div className="move-history shadow border bg-white rounded pl-12 pt-4 pr-10 mt-2 min-h-[50rem]">
-      <h2 className="text-xl font-bold">Move History</h2>
+    <div className="move-history shadow border bg-white rounded ml-4 pt-2 mt-2 min-h-[50rem] pr-[5rem] text-center">
       <table className="min-w-full">
         <thead>
           <tr>
