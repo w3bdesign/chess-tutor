614fb87391fba4d45c7758a53a4661892510e26a
Daniel <45217974+w3bdesign@users.noreply.github.com>
2024-07-13 00:32:22 +0200

    Update App.jsx

--- chess-tutor/src/App.jsx
+++ chess-tutor/src/App.jsx
@@ -1,10 +1,13 @@
 import ChessBoard from "./components/ChessBoard";
 import MoveHistory from "./components/MoveHistory";
 
+import useChessStore from "./stores/useChessStore";
+
 import "./App.css";
 import "@chrisoakman/chessboard2/dist/chessboard2.min.css";
 
 function App() {
+  const resetGame = useChessStore((state) => state.resetGame);
   return (
     <div className="container mx-auto p-4">
       <div className="shadow border bg-white rounded w-full mb-4">
@@ -13,6 +16,12 @@ function App() {
       <div className="flex flex-col lg:flex-row gap-4">
         <div className="lg:w-2/3">
           <ChessBoard />
+          <button
+            onClick={resetGame}
+            className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Reset Game
+          </button>
         </div>
         <div className="lg:w-1/3">
           <MoveHistory />
