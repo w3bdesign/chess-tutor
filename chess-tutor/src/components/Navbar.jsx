7c24cbede83c40ae345f8f8527423bf55508f0cc
Daniel <45217974+w3bdesign@users.noreply.github.com>
2024-07-27 08:00:08 +0200

    Navbar

--- chess-tutor/src/components/Navbar.jsx
+++ chess-tutor/src/components/Navbar.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 p-4">
-      <ul className="flex space-x-4">
-        <li>
-          <a href="#analysis" className="text-white hover:text-gray-400">Analysis</a>
+    <nav className="bg-gray-800 p-2 mb-4">
+      <ul className="flex space-x-2 p-2 ">
+        <li className="border border-white p-2 rounded bg-gray-600 shadow">
+          <a href="#analysis" className="text-white hover:text-gray-300 transition duration-300 ease-in-out">
+            Analysis
+          </a>
         </li>
-        <li className="hidden">
-          <a href="#puzzles" className="text-white hover:text-gray-400">Puzzles</a>
+        <li className="border border-white p-2 rounded bg-gray-600 shadow border hidden">
+          <a href="#puzzles" className="text-white hover:text-gray-300 transition duration-300 ease-in-out">
+            Puzzles
+          </a>
         </li>
       </ul>
     </nav>
