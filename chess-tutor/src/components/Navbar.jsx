0e81c3c0740dccb626ce436c902428b28b9bc34a
Daniel <45217974+w3bdesign@users.noreply.github.com>
2024-07-27 08:35:21 +0200

    Update Navbar.jsx

--- chess-tutor/src/components/Navbar.jsx
+++ chess-tutor/src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className=" bg-slate-200 rounded shadow p-2 mb-4">
-      <ul className="flex space-x-2 p-2">
-        <li className="group p-2 rounded hover:bg-gray-500 transition duration-300 ease-in-out hover:text-gray-100">
+      <ul className="flex space-x-2 p-2 ">
+        <li className="group p-2 rounded hover:bg-slate-400 transition duration-300 ease-in-out hover:text-gray-100">
           <a
             href="#analysis"
             className="text-gray-900 group-hover:text-gray-100 transition duration-300 ease-in-out"
@@ -12,10 +12,10 @@ const Navbar = () => {
             Analysis
           </a>
         </li>
-        <li className="border border-gray-600 p-2 rounded bg-gray-500 shadow border hidden">
+        <li className="hidden group p-2 rounded hover:bg-slate-400 transition duration-300 ease-in-out hover:text-gray-100">
           <a
             href="#puzzles"
-            className="text-gray-900 hover:text-gray-100 transition duration-300 ease-in-out"
+            className="text-gray-900 group-hover:text-gray-100 transition duration-300 ease-in-out"
           >
             Puzzles
           </a>
