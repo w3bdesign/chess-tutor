1e3390b8079d5accc83f9bf592b435057d4839d8
Daniel <45217974+w3bdesign@users.noreply.github.com>
2024-07-28 05:15:57 +0200

    Update puzzleService.js

--- chess-tutor/src/services/puzzleService.js
+++ chess-tutor/src/services/puzzleService.js
@@ -1,29 +1,10 @@
-// https://rapidapi.com/KeeghanM/api/chess-puzzles/playground/apiendpoint_177d3683-dcc4-4a7b-812a-884e259e1304
-
-import axios from 'axios';
-import { Chess } from 'chess.js';
+import axios from "axios";
+import { Chess } from "chess.js";
 
 export const fetchPuzzle = async () => {
   try {
-    const options = {
-      method: 'GET',
-      url: 'https://chess-puzzles.p.rapidapi.com/',
-      params: {
-        themes: '["middlegame","advantage"]',
-        rating: '1500',
-        themesType: 'ALL',
-        playerMoves: '4',
-        count: '1'
-      },
-      headers: {
-        // TODO: Move this to /api endpoint
-        'x-rapidapi-key': '',
-        'x-rapidapi-host': 'chess-puzzles.p.rapidapi.com'
-      }
-    };
-
-    const response = await axios.request(options);
-    const puzzleData = response.data.puzzles[0];
+    const response = await axios.get("/api/chess-puzzle");
+    const puzzleData = response.data;
 
     // Create a new Chess instance
     const chess = new Chess();
@@ -37,7 +18,7 @@ export const fetchPuzzle = async () => {
       themes: puzzleData.themes,
     };
   } catch (error) {
-    console.error('Error fetching puzzle:', error);
+    console.error("Error fetching puzzle:", error);
     throw error;
   }
 };
