967804d9ed8370b9df05997870607bf4e4ea2846
Daniel <45217974+w3bdesign@users.noreply.github.com>
2024-07-28 05:18:36 +0200

    isDev

--- chess-tutor/src/services/puzzleService.js
+++ chess-tutor/src/services/puzzleService.js
@@ -1,10 +1,41 @@
+// File: src/services/puzzleService.js
+
 import axios from "axios";
 import { Chess } from "chess.js";
 
+const isDevelopment = import.meta.env.MODE === "development";
+
+const fetchPuzzleFromAPI = async () => {
+  const response = await axios.get("/api/chess-puzzle");
+  return response.data;
+};
+
+const fetchPuzzleDirectly = async () => {
+  const options = {
+    method: "GET",
+    url: "https://chess-puzzles.p.rapidapi.com/",
+    params: {
+      themes: '["middlegame","advantage"]',
+      rating: "1500",
+      themesType: "ALL",
+      playerMoves: "4",
+      count: "1",
+    },
+    headers: {
+      "x-rapidapi-key": import.meta.env.VITE_RAPID_API_KEY,
+      "x-rapidapi-host": "chess-puzzles.p.rapidapi.com",
+    },
+  };
+
+  const response = await axios.request(options);
+  return response.data.puzzles[0];
+};
+
 export const fetchPuzzle = async () => {
   try {
-    const response = await axios.get("/api/chess-puzzle");
-    const puzzleData = response.data;
+    const puzzleData = isDevelopment
+      ? await fetchPuzzleDirectly()
+      : await fetchPuzzleFromAPI();
 
     // Create a new Chess instance
     const chess = new Chess();
